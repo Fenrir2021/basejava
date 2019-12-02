0e7d57103345925b63b5472a27a6f47523ab9e71
Fenrir <vladimir.potapov.ru@yandex.ru>
2019-12-03 04:40:22 +0900

    working!

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,21 +1,21 @@
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    public int length = storage.length;
-    public int size = 1;
+    private Resume[] storage = new Resume[10000];
+    private int length = storage.length;
+    private int size = 0;
+    //private Resume uuid;
 
-    void clear() {
+    void clear() {                                                             // ++
         for (int i = 0; i < length; i++) {
             storage[i] = null;
         }
         System.out.println("Cleared!");
+        size = 0;
     }
 
-    void save(Resume r) {
+    void save(Resume r) {                                                     // ++
         for (int i = 0; i < length; i++) {
             if (storage[i] == null) {
                 storage[i] = r;
@@ -28,19 +28,31 @@ public class ArrayStorage {
 
     }
 
-    String get(String uuid) {
-        return uuid; //storage[uuid];
+    String get(String uuid) {                                                    // ++
+
+
+        int a = 0;
+        for (int i = 0; i < length; i++) {
+            if (String.valueOf(storage[i]).equals(uuid)) {
+
+                //System.out.println(storage[i]);
+                // System.out.println("getting! ");
+                a = i;
+               // size++;
+            }else return uuid;
+
+        }
+        return String.valueOf(storage[a]); //storage[uuid];
     }
 
-    void delete(String uuid) {
-        // for (int i=0; i< length; i++) {
-        //   if (storage[i] = uuid){
-        //       storage[i] = null;
-        //   }
-        //storage[uuid] = null;
-        System.out.println("item deleted");
-        size--;
-        // }
+    void delete(String uuid) {                                                  // ++
+        for (int i = 0; i < length; i++) {
+            if (String.valueOf(storage[i]).equals(uuid)) {
+                storage[i] = null;
+                System.out.println("item deleted");
+                size--;
+            }
+        }
 
     }
 
@@ -48,14 +60,24 @@ public class ArrayStorage {
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
-        String getAll;
-        getAll = Arrays.toString(storage);
-        System.out.println(getAll);
+    Resume[] getAll() {                                                          // ++
+        //String getAll;
+        //getAll = Arrays.toString(storage);
+        for (Resume s : storage
+        ) {
+            if (s != null) {
+                System.out.print(s + " ");
+
+            }
+
+
+        }
+        System.out.println();
+
         return new Resume[0];
     }
 
-    int size() {
+    int size() {                                                                 // ++
         // int lenArr = 0;
         // for (int i = 0; i < length; i++) {
         //     if (storage[i] != null) ++lenArr;
--- src/MainArray.java
+++ src/MainArray.java
@@ -24,26 +24,26 @@ public class MainArray {
                 uuid = params[1].intern();
             }
             switch (params[0]) {
-                case "list":
+                case "list":                                               // ++
                     printAll();
                     break;
-                case "size":
+                case "size":                                               // ++
                     System.out.println(ARRAY_STORAGE.size());
                     break;
-                case "save":
+                case "save":                                               // ++
                     r = new Resume();
                     r.uuid = uuid;
                     ARRAY_STORAGE.save(r);
                     printAll();
                     break;
-                case "delete":
+                case "delete":                                             //++
                     ARRAY_STORAGE.delete(uuid);
                     printAll();
                     break;
-                case "get":
+                case "get":                                                // ++
                     System.out.println(ARRAY_STORAGE.get(uuid));
                     break;
-                case "clear":
+                case "clear":                                              // ++
                     ARRAY_STORAGE.clear();
                     printAll();
                     break;
@@ -56,7 +56,7 @@ public class MainArray {
         }
     }
 
-    static void printAll() {
+    private static void printAll() {
         Resume[] all = ARRAY_STORAGE.getAll();
         System.out.println("----------------------------");
         if (all.length == 0) {
