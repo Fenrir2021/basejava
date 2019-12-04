7071cbb2bbdb1069996efd921c5be224dcc8744a
Fenrir <vladimir.potapov.ru@yandex.ru>
2019-12-04 20:51:34 +0900

    правильный get() и delete()

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,9 +3,7 @@
  */
 public class ArrayStorage {
     private Resume[] storage = new Resume[10000];
-    //private int length = storage.length;
     private int size = 0;
-    //private Resume uuid;
 
     void clear() {                                                             // ++
         for (int i = 0; i < size; i++) {
@@ -16,35 +14,24 @@ public class ArrayStorage {
     }
 
     void save(Resume r) {                                                     // ++
-        // for (int i = 0; i < length; i++) {
-        //    if (storage[i] == null) {
         storage[size] = r;
-        // System.out.println("saved ");
         size++;
-        //return;
-        //}
-        //}
     }
 
     Resume get(String uuid) {                                       // ++
-        int a = 0;
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 //System.out.println(storage[i]);
                 // System.out.println("getting! ");
                 return storage[i];
             }
-            a = i;
         }
-        if (a > size) {
-            return null;
-        }
-        return storage[a];
+        return null;
     }
 
     void delete(String uuid) {                                                  // ++
         for (int i = 0; i < size; i++) {
-            if (String.valueOf(storage[i]).equals(uuid)) {
+            if (storage[i].uuid.equals(uuid)) {
                 storage[i] = null;
                 System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
                 System.out.println("item deleted");
